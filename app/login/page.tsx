bd1e5319702f0d137db0b385d8708c337fcc10d6
MahinAshraful <150500453+MahinAshraful@users.noreply.github.com>
2024-08-07 16:49:15 -0400

    login text is now visible

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,9 +4,9 @@ export default function LoginPage() {
   return (
     <form>
       <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
+      <input id="email" name="email" type="email" className='text-black' required />
       <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
+      <input id="password" name="password" type="password" className='text-black' required />
       <button formAction={login}>Log in</button>
       <button formAction={signup}>Sign up</button>
     </form>
